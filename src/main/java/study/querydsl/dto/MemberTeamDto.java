fe300091f96941acc5de2eeef10c59445b769738
seongjin_song <seongjin_song@tmax.co.kr>
2022-01-30 00:23:54 +0900

    동적 쿼리와 성능 최적화 조회 - Builder 사용

---
+++ src/main/java/study/querydsl/dto/MemberTeamDto.java
@@ -0,0 +1,22 @@
+package study.querydsl.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+@Data
+public class MemberTeamDto {
+    private Long memberId;
+    private String username;
+    private int age;
+    private Long teamId;
+    private String teamName;
+
+    @QueryProjection // dto가 순수하지 못하고 querydsl 라이브러리에 의존하게 된다!
+    public MemberTeamDto(Long memberId, String username, int age, Long teamId, String teamName) {
+        this.memberId = memberId;
+        this.username = username;
+        this.age = age;
+        this.teamId = teamId;
+        this.teamName = teamName;
+    }
+}
--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 
@@ -49,5 +52,38 @@ class MemberJpaRepositoryTest {
 
         List<Member> result2 = memberJpaRepository.findByUsername_querydsl("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    void searchByBuilderTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+//        condition.setAgeGoe(35);
+//        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        /**
+         * 조건이 없으면 데이터를 다 끌고온다.
+         * 운영하다 보면 데이터가 많아진다.
+         * 왠만하면 조건이 있고, 페이징도 같이 들어가 있어야 한다.
+         */
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+//        assertThat(result).extracting("username").containsExactly("member4");
+        assertThat(result).extracting("username").containsExactly("member3", "member4");
+
     }
 }
